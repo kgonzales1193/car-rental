d07289fb89d36113c5a7415d9178248236db27e9
kgonzales1193 <kgonzales1193@gmail.com>
2023-06-13 17:21:32 +0800

    fixed error on imports at SearchManufacturer.tsx

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { Combobox, Transition } from "@headlessui/react";
-import { SearchManufacturerProps } from "@/types";
+
 import { manufacturers } from "@/constants";
 import { useState, Fragment } from "react";
 import Image from "next/image";
+import { SearchManuFacturerProps } from "@/types";
 
 const SearchManufacturer = ({
   manufacturer,
-  setManufacturer,
-}: SearchManufacturerProps) => {
+  setManuFacturer,
+}: SearchManuFacturerProps) => {
   const [query, setQuery] = useState("");
 
   const filteredManufacturers =
@@ -24,7 +25,7 @@ const SearchManufacturer = ({
 
   return (
     <div className='search-manufacturer'>
-      <Combobox value={manufacturer} onChange={setManufacturer}>
+      <Combobox value={manufacturer} onChange={setManuFacturer}>
         <div className='relative w-full'>
           <Combobox.Button className='absolute top-[14px]'>
             <Image
